c331752af9001a7fde3f12c87475f8d2cd2f3054
Rafael Canto <rafaelcanto@outlook.com>
2022-08-04 10:04:41 -0300

    initial

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "test" {
+  cidr_block = "10.199.199.0/24"
+  tags = {
+    "description" = "vpc de teste do iac"
+  }
+}
+
